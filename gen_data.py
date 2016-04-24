c92a8e8f0da46e72260b10b91c0a91ce467e3813
Adam P Stallard <stallard@linprog4.cs.fsu.edu>
2016-04-24 18:11:05 -0400

    Added synthetic data set generator

---
+++ gen_data.py
@@ -0,0 +1,42 @@
+import os, sys
+from random import randint as ri
+from math import sqrt
+
+
+maxW = 1000
+maxL = 1000
+
+def main():
+    if len(sys.argv) < 2:
+        print 'Please provide input parameter n'
+        sys.exit()
+    else:
+        try:
+            n = int(sys.argv[1])
+            if n < 1:
+                raise
+        except:
+            print '1st Argument must be valid positive integer'
+            sys.exit()
+
+    matrix = gen(n)
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            sys.stdout.write(str(matrix[i][j]) + ' ')
+        sys.stdout.write('\n')
+
+def gen(n):
+    global maxW, maxL
+    pos = [ (ri(0, maxW), ri(0, maxL)) for _ in range(n) ]
+    M = [ [] for _ in range(n) ] * n
+    for i in range(n):
+        for j in range(n):
+            M[i].append(dist(pos[i], pos[j]))
+    return M
+
+def dist(x, y):
+    return int(sqrt((x[0] - y[0])**2 + ((x[1] - y[1]) ** 2)))
+
+
+if __name__ == '__main__':
+    main()
